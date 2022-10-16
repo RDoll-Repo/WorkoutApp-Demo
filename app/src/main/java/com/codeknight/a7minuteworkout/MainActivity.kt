d9738f53bd0c9f8e67cdb33392c9da71d4445cf2
Roland Doll <rdoll007@gmail.com>
2022-10-16 13:53:40 -0400

    progress bar UI

---
+++ app/src/main/java/com/codeknight/a7minuteworkout/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.codeknight.a7minuteworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.codeknight.a7minuteworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
