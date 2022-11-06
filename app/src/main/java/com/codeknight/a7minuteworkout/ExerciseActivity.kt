3e4f93eb20515d3003b37da5fa8bbd2e259373d5
Roland Doll <rdoll007@gmail.com>
2022-11-05 21:09:31 -0400

    add resources

--- app/src/main/java/com/codeknight/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/codeknight/a7minuteworkout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.codeknight.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.codeknight.a7minuteworkout.databinding.ActivityExerciseBinding
 
@@ -10,7 +11,10 @@ class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
 
     private var restTimer: CountDownTimer? = null
-    var restProgress = 0
+    private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +43,11 @@ class ExerciseActivity : AppCompatActivity() {
             restProgress = 0
         }
 
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
         binding = null
     }
 
@@ -51,6 +60,19 @@ class ExerciseActivity : AppCompatActivity() {
         setRestProgressBar()
     }
 
+    private fun setUpExerciseView() {
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flExerciseView?.visibility = View.VISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgressBar()
+    }
+
     private fun setRestProgressBar() {
         binding?.progressBar?.progress = restProgress
         restTimer = object: CountDownTimer(10000, 1000) {
@@ -61,13 +83,27 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                setUpExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding?.progressBarExercise?.progress = exerciseProgress
+        exerciseTimer = object: CountDownTimer(30000, 1000) {
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
                 Toast.makeText(
                     this@ExerciseActivity,
-                    "Finished!",
+                    "Exercise Finished!",
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         }.start()
     }
 }
